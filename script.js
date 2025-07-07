ca79235085b1d0b355f9df76e8e42952300da9c3
habiburrahman63 <habiburrahman90763@gamil.com>
2025-07-07 23:15:08 +0600

    loop add

---
+++ script.js
@@ -0,0 +1,53 @@
+
+// for(let i = 10; i >= 1; i--){
+//   console.log(i)
+// }
+
+
+
+// let  i = 10;
+
+// while(i >= 1){
+//     console.log(i)
+//     i--
+// }
+
+// let i = 10;
+
+// do{
+//     console.log(i)
+//     i--
+// }while(i>=1)
+
+
+
+
+
+
+let start = 9; 
+
+for(let namta = start; namta <= start + 2; namta++){
+    for(let i = 1; i <= 10; i++){
+        document.write(namta + " X " + i + " = " + (namta * i) + "<br>");
+    }
+    document.write("<br>");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
