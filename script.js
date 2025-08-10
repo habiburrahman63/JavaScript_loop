531a2212b9aaa8a5c2514e4e242e7882ac0dc028
habiburrahman63 <habiburrahman90763@gamil.com>
2025-08-10 17:24:40 +0600

    new update

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-const Btn = document.querySelector(".Btn")
-const output = document.querySelector(".output")
+const Btn = document.querySelector(".Btn");
+const output = document.querySelector(".output");
 
-Btn.addEventListener("click", function(){
-    const inputFild = document.querySelector(".inputFild").value;
+Btn.addEventListener("click", function () {
+  const inputFild = document.querySelector(".inputFild").value;
 
-    const passwordLenth = 
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_+=/.,<>?;:\|"
-    let password = ''
-if(true){
-        for(let i = 0; i < inputFild; i++){
-        const random = (Math.round(Math.random()*passwordLenth.length))
-        const randomPass = passwordLenth[random]
-        
-        password += randomPass;
-        output.innerHTML = password;
-        
-    }
-}
-    
-    
-})
+  const passwordLenth =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!@#$%^&*()_+=/.,<>?;:|";
+  let password = "";
+  for (let i = 0; i < inputFild; i++) {
+    const random = Math.round(Math.random() * passwordLenth.length);
+    const randomPass = passwordLenth[random];
+
+    password += randomPass;
+    output.innerHTML = password;
+  }
+  document.querySelector(".inputFild").value = "";
+});
